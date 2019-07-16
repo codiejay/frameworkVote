8c3b8b1ba9c8eabde1c07a71c3225fc9e1bd7b43
0zzycodes <horlalekan123@gmail.com>
2019-07-16 20:45:32 +0800

    Changed function componet in app js to class componet

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Landing.css';
+
+class Landing extends React.Component() {
+  render() {
+    return (
+      <div className="landingPage">
+        <h2>AYE, THERE DEV!</h2>
+        <p>
+          It time to vote for your fav framework. But before that, let’s know
+          your name!{' '}
+        </p>
+        <div className="usersName">
+          <input placeholder="Your Name!" />
+        </div>
+
+        <div id="userSocialMedia">
+          <p>Optional</p>
+          <section>
+            <input placeholder="Instagram Handle" />
+            <input placeholder="Twitter Handle" />
+          </section>
+          <div id="goButton">
+            <button> Let's Go!>> </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
