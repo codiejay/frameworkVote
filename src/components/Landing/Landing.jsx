3d7d4cd222b934a1ad3720d3293541a27774e41f
0zzycodes <horlalekan123@gmail.com>
2019-07-18 03:48:18 +0800

    fixed

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../Landing/Landing.css';
-const Landing = ({ handleSubmit }) => {
-  return (
-    <div className="landingPage">
-      <h2>AYE, THERE DEV!</h2>
-      <p>
-        It time to vote for your fav framework. But before that, let’s know your
-        name!{' '}
-      </p>
-      <div className="usersName">
-        <input placeholder="Your Name!" />
-      </div>
+import Input from '../UI/Input/Input';
+import Button from '../UI/Button/Button';
 
-      <div id="userSocialMedia">
-        <p>Optional</p>
-        <section>
-          <input placeholder="Instagram Handle" />
-          <input placeholder="Twitter Handle" />
-        </section>
-        <div id="goButton">
-          <button onClick={handleSubmit}> Let's Go!>> </button>
+export default class Landing extends Component {
+  vote = () => {
+    this.props.history.push('/vote');
+  };
+  render() {
+    return (
+      <div className="landingPage">
+        <h2>AYE, THERE DEV!</h2>
+        <p>
+          It time to vote for your fav framework. But before that, let’s know
+          your name!
+        </p>
+        <div className="usersName">
+          <Input inputType="text" placeholder="Your Name!" />
+        </div>
+
+        <div id="userSocialMedia">
+          <p>Optional</p>
+          <section>
+            <Input inputType="text" placeholder="Instagram Handle" />
+            <Input inputType="text" placeholder="Twitter Handle" />
+          </section>
+          <div id="goButton">
+            <Button clicked={this.vote}>Let's Go!</Button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Landing;
+    );
+  }
+}
