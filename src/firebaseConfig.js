503a81f6595b808226e4dbd8da412c465a26ff27
0zzycodes <horlalekan123@gmail.com>
2019-07-19 06:35:27 +0800

    changed firebase config object

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBmzuiCPGdvKkwwrsyHCq3J3mFjSm1VeMs',
-  authDomain: 'frameworktest-4759b.firebaseapp.com',
-  databaseURL: 'https://frameworktest-4759b.firebaseio.com',
-  projectId: 'frameworktest-4759b',
-  storageBucket: 'frameworktest-4759b.appspot.com',
-  messagingSenderId: '453912383853',
-  appId: '1:453912383853:web:0d9598cfbc27de7f'
+  apiKey: 'AIzaSyDJLjU8GK6RbaZ7ISa9qk08JQRtCPp8L7M',
+  authDomain: 'frameworkvoting.firebaseapp.com',
+  databaseURL: 'https://frameworkvoting.firebaseio.com',
+  projectId: 'frameworkvoting',
+  storageBucket: 'frameworkvoting.appspot.com',
+  messagingSenderId: '602552600472',
+  appId: '1:602552600472:web:814b80827372362a'
 };
 
 var app = firebase.initializeApp(firebaseConfig);
